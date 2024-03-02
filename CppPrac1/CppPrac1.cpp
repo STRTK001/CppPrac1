cdb3338683a69a549c46601f5ffc97b745ff9c97
Travis Strawbridge <strtk001@mymail.unisa.edu.au>
2024-03-02 14:51:39 +1030

    setup for prac1 part 2

--- CppPrac1/CppPrac1.cpp
+++ CppPrac1/CppPrac1.cpp
@@ -12,13 +12,32 @@ Note: function1 expects an int type, whereas function2 expects a pointer to an i
 void function1(int);
 void function2(int*);
 
-int main() {
+void part1();
+void prac2();
+
+/* function to calculate the sum of two numbers */
+int calculateSum(int, int);
+/* function to display details to the screen */
+void displayDetails();
+
+int main() 
+{
+	std::cout << "__ PART 1 __ \n";
+	prac1();
+	std::cout << "__ PART 2 __ \n";
+	prac2();
+
+	return 0;
+}
+
+void prac1()
+{
 	int number = 10;
 	/* C1. display the contents of number to the screen */
 	std::cout << "1. The number is " << number << std::endl;
 
 	/* C2. display the address of variable number to the screen */
-	std::cout << "2. The address of the number var is " << &number  << std::endl;
+	std::cout << "2. The address of the number var is " << &number << std::endl;
 
 	/* C3. call function1 */
 	function1(number);
@@ -32,8 +51,6 @@ int main() {
 
 	/* C6. display the contents of number to the screen */
 	std::cout << "9. The number after function2 is " << number << std::endl;
-
-	return 0;
 }
 
 void function1(int number)
@@ -60,4 +77,42 @@ void function2(int* numberPtr)
 
 	/* C13. display the contents of number to the screen */
 	std::cout << "8. The number ptr value after incremented is " << *numberPtr << std::endl;
+}
+
+void prac2()
+{
+	int num1 = 0; /* first number to be entered by user */
+	int num2 = 0; /* second number to be entered by user */
+	int sum = 0; /* stores sum of two numbers */
+	displayDetails();
+	/* prompt for and read first number */
+	std::cout << "Please enter first number : ";
+	num1 = getNumber();
+	/* prompt for and read second number */
+	std::cout << "Please enter second number: ";
+	num2 = getNumber();
+	/* add two numbers together and display to screen */
+	sum = calculateSum(num1, num2);
+	std::cout << std::endl << "\nSum of " << num1 << " and " << num2 << " is: " <<
+	sum << std::endl << std::endl;
+}
+
+/* function to read number from standard input (keyboard) */
+int getNumber() 
+{
+	int num;
+	std::cin >> num;
+	return num;
+}
+/* function to calculate the sum of two numbers */
+int calculateSum(int number1, int number2) 
+{
+	return number1 + number2;
+}
+/* function to display details to the screen */
+void displayDetails() 
+{
+	std::cout << "Author : Your Name" << std::endl;
+	std::cout << "Stud ID : Your Student Id" << std::endl;
+	std::cout << "Email : Your Email" << std::endl;
 }
