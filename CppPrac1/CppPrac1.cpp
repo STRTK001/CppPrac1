ec848f51de4b4c152cc3b374b02a55ae3aafadb5
Travis Strawbridge <strtk001@mymail.unisa.edu.au>
2024-03-02 14:47:12 +1030

    completed prac1 part 1

--- CppPrac1/CppPrac1.cpp
+++ CppPrac1/CppPrac1.cpp
@@ -15,24 +15,49 @@ void function2(int*);
 int main() {
 	int number = 10;
 	/* C1. display the contents of number to the screen */
+	std::cout << "1. The number is " << number << std::endl;
+
 	/* C2. display the address of variable number to the screen */
+	std::cout << "2. The address of the number var is " << &number  << std::endl;
+
 	/* C3. call function1 */
+	function1(number);
+
 	/* C4. display the contents of number to the screen */
+	std::cout << "5. The number after function1 is " << number << std::endl;
+
+	int* numPtr = &number;
 	/* C5. call function2 */
+	function2(numPtr);
+
 	/* C6. display the contents of number to the screen */
+	std::cout << "9. The number after function2 is " << number << std::endl;
+
 	return 0;
 }
+
 void function1(int number)
 {
 	/* C7. increment number by one */
+	number++;
 	/* C8. display the contents of number to the screen (should be 11) */
+	std::cout << "3. The incremented number is " << number << std::endl;
 	/* C9. display the address of number to the screen */
+	std::cout << "4. and its address is  " << &number << std::endl;
+	
 }
 void function2(int* numberPtr)
 {
 	/* C10. display the address of number to the screen (should be the same
 	address as you displayed first in main) */
+	std::cout << "6. The number ptr address is " << numberPtr << std::endl;
+
 	/* C11. display the value that the pointer numberPtr points to */
+	std::cout << "7. The number ptr value is " << *numberPtr << std::endl;
+
 	/* C12. increment the value stored in number */
+	(*numberPtr)++;
+
 	/* C13. display the contents of number to the screen */
+	std::cout << "8. The number ptr value after incremented is " << *numberPtr << std::endl;
 }
